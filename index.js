4bc2e610705d8bc1fac6c29b5733de8b4b01f1c0
PorlanMarket <rsrsrsporlan@gmail.com>
2025-01-26 19:32:57 +0700

    index.js

--- index.js
+++ index.js
@@ -34,14 +34,12 @@ app.get('/', (req, res) => {
   res.sendFile(imagePath);
 });
 app.listen(port, () => {
-  console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
-});
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
-const statusTypes = [ 'dnd', 'idle'];
-let currentStatusIndex = 0;
+  
+const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing Porlan"];
+
+
 let currentTypeIndex = 0;
-
 async function login() {
   try {
     await client.login(process.env.TOKEN);
